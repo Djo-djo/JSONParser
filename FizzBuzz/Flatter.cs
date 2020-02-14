60f9a6b342c8177c3057104dda1ac9297a0b53c3
Djo-djo <pashalazovskii@gmail.com>
2020-02-14 14:16:45 +0200

    Complete

---
+++ FizzBuzz/Flatter.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    class Flatter
+    {
+        #region Fields
+        private static List<string> jsonKeys = new List<string>();
+        private static List<object> jsonValues = new List<object>();
+        #endregion
+
+        #region Property
+        /// <summary>
+        /// Returns JSON string that represents keys array
+        /// </summary>
+        public static string JsonKeys => JsonConvert.SerializeObject(jsonKeys, Formatting.Indented);
+
+        /// <summary>
+        /// Returns JSON string that represents values array
+        /// </summary>
+        public static string JsonValues => JsonConvert.SerializeObject(jsonValues, Formatting.Indented);
+
+        public static List<string> GetKeys => jsonKeys;
+
+        public static List<object> GetValues => jsonValues;
+        #endregion
+
+        #region Methods
+        public static void Parse(string jsonText)
+        {
+            JObject jObject = JObject.Parse(jsonText);
+            Dictionary<string, object> nodes = new Dictionary<string, object>();
+
+            FillDictionaty(jObject, nodes);
+
+            jsonKeys.AddRange(nodes.Keys);
+            jsonValues.AddRange(nodes.Values);
+        }
+
+        private static void FillDictionaty(JToken jToken, Dictionary<string, object> nodes, string parentNode = "")
+        {
+            if (jToken.HasValues)
+            {
+                int i = 0;
+                foreach (var child in jToken.Children())
+                {
+                    if (jToken.Type == JTokenType.Property)
+                    {
+                        parentNode = parentNode.Equals(string.Empty) ? ((JProperty)jToken).Name : $"{parentNode}.{((JProperty)jToken).Name}";
+                    }
+                    FillDictionaty(child, nodes, jToken.Type == JTokenType.Array ? $"{parentNode}.[{i++}]" : parentNode);
+                }
+            }
+            else
+            {
+                nodes.Add(parentNode, ((JProperty)jToken.Parent).Value);
+            }
+        }
+        #endregion
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonText = "{" +
+                "\"Number\": 7," +
+                "\"Md\": 120," +
+                "\"Pressure\": 39500000," +
+                "\"Density\": {" +
+                    "\"Liquid\": 518," +
+                    "\"Gas\": 50}," +
+                "\"MassFractions\": {" +
+                    "\"Liquid\": [" +
+                        "{\"Key\": \"6276c48e-83d9-47ec-8d69-5aa623abe950\"," +
+                        "\"Value\": 0.3}" +
+                        "]," +
+                    "\"Gas\": [" +
+                        "{\"Key\": \"16abc7fb-5bde-4619-9c36-2329fbc63923\"," +
+                        "\"Value\": 0.5}]" +
+                    "}" +
+                "}";
+
+            Flatter.Parse(jsonText);
+            Console.WriteLine(Flatter.JsonKeys);
+            Console.WriteLine();
+            Console.WriteLine(Flatter.JsonValues);
+        }
+    }
+}
